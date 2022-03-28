9e07b1e41cea85b6edbf3dd9c2a5058cefbb8a02
kimchulyeon <guinness987@gmail.com>
2022-03-29 03:31:18 +0900

    feat: install packages & developing sidebar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import styled from '@emotion/styled'
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Sidebar from './components/sidebar/Sidebar'
+import Home from './pages/home/Home'
+
+const Container = styled.div`
+  display: flex;
+`
 
 const App = () => {
-  return <div></div>
+  return (
+    <Container>
+      <Sidebar />
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="" element={<></>} />
+        </Route>
+      </Routes>
+    </Container>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from '@emotion/react'
+import { defaultTheme } from './utils/theme'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 )
